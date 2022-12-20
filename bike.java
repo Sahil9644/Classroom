ab85cf339d6ef182dbe9d76692eb52711d7debc2
Sahil9644 <120358533+Sahil9644@users.noreply.github.com>
2022-12-20 14:02:21 +0530

    Add files via upload

---
+++ bike.java
@@ -0,0 +1,54 @@
+package classwork;
+
+import java.util.Scanner;
+
+class details
+{
+	String bikec,model,no;
+	public details(String bikec,String model,String no)
+	{
+		this.bikec=bikec;
+		this.model=model;
+		this.no=no;
+	}
+	void getData() {
+		System.out.println("Vehicle Company:"+bikec+"\n"+"Vehicle model:"+model+"Vehicle number"+no);
+		
+	}
+
+}
+class assign extends details
+{
+	String type,dname,dl;
+	public assign(String bikec,String model,String no,String type,String dname,String dl)
+	{
+		super(bikec,model,no);
+		this.type=type;
+		this.dname=dname;
+		this.dl=dl;
+	}
+	void setData()
+	{
+		super.getData();
+		System.out.println("Vehicle type: "+type+"\n"+"Driver name: "+dname+"\nDriving License: "+dl);
+		
+	}
+}
+public class bike
+{
+	public static void main(String[] args) {
+		String bikec,model,no,dname,dl,type;
+		System.out.println("enter 1:For 4wheeler \n2:For 2wheeler");
+		Scanner sc=new Scanner(System.in);
+		type=sc.nextLine();
+		System.out.println("Enter Vehicle company \nmodel \nnumber");
+		bikec=sc.nextLine();
+		model=sc.nextLine();
+		no=sc.nextLine();
+		System.out.println("Enter driver Details: \nName: \nDriving License: ");
+		dname=sc.nextLine();
+		dl=sc.nextLine();
+		assign g=new assign(bikec,model,no,type,dname,dl);
+		g.setData();
+	}
+	}
